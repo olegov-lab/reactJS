449a61be6908768e9fe7c468a3cbae648bd41089
oleg kochetov <olego8760@gmail.com>
2023-01-28 11:34:13 +0300

    init:start project moved window

---
+++ movedmenu/src/MenuContainer.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class MenuContainer extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <p>What's item?</p>
+                    <ul>
+                        <li>Lorem</li>
+                        <li>Flow</li>
+                        <li>Pol</li>
+                        <li>Sit</li>
+                        <li>Moreder</li>
+                        <li>Modern</li>
+                        <li>Nothing</li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MenuContainer;
