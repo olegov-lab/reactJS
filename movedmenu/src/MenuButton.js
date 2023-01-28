9c7b0ef550f7462a106760607dea8073a581853a
oleg kochetov <olego8760@gmail.com>
2023-01-28 12:24:20 +0300

    feat: Create Menu component, MenuButton component

---
+++ movedmenu/src/MenuButton.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./MenuButton.css";
+
+class MenuButton extends Component {
+    render() {
+        return (
+            <button id="roundButton" onMouseDown={this.props.handleMoseDown} ></button>
+        )
+    }
+}
+
+export default MenuButton;
--- movedmenu/src/MenuContainer.js
+++ movedmenu/src/MenuContainer.js
@@ -1,9 +1,41 @@
 import React, { Component } from "react";
+import MenuButton from "./MenuButton";
+import Menu from "./Menu";
 
 class MenuContainer extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            visible: false
+        };
+
+        this.tooggleMenu = this.tooggleMenu.bind(this);
+        this.handleMoudeDown = this.handleMoudeDown.bind(this);
+    }
+
+    tooggleMenu() {
+        this.setState({
+            visible: !this.state.visible
+        });
+    }
+
+    handleMoudeDown(e) {
+        this.tooggleMenu();
+
+        console.log('clicked');
+
+        //e.stopPropagaton();
+    }
+
     render() {
         return (
             <div>
+                <MenuButton handleMoseDown={this.handleMoudeDown} />
+                <Menu handleMoseDown={this.handleMoudeDown}
+                    menuVisibility={this.state.visible} />
+
                 <div>
                     <p>What's item?</p>
                     <ul>
